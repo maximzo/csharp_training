a5af789cfb31f82c886457dfc3a08364d40c6149
maximzo <maxburn@ya.ru>
2021-12-16 01:33:38 +0400

    Лекция 9.1. Загрузка файлов на удалённый сервер по протоколу FTP

---
+++ mantis-tests/mantis-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("mantis-tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("mantis-tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("64e6137a-1a84-4871-8898-365a3af50336")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ mantis-tests/mantis-tests/appmanager/AppManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace mantis_tests
+{
+    public class AppManager
+    {
+        protected IWebDriver driver;
+        protected string baseURL;
+
+        public RegistrationHelper Registration { get; set; }
+        public FtpHelper Ftp { get; set; }
+
+        private static ThreadLocal<AppManager> app = new ThreadLocal<AppManager>();
+
+        private AppManager()
+        {
+            driver = new ChromeDriver();
+            baseURL = "http://localhost";
+            Registration = new RegistrationHelper(this);
+            Ftp = new FtpHelper(this);
+        }
+
+        ~AppManager()
+        {            
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+
+        public static AppManager GetInstance()
+        {
+            if (! app.IsValueCreated)
+            {
+                AppManager newInstance = new AppManager();
+                newInstance.driver.Url = "http://localhost/mantisbt/login_page.php";
+                app.Value = newInstance;
+            }
+            return app.Value;
+        }
+
+        public IWebDriver Driver
+        {
+            get
+            {
+                return driver;
+            }
+        }
+    }
+}
